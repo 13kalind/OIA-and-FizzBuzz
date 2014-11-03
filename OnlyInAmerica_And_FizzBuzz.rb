d16194a811931a1e59ebee5db770fae2734a5a13
Daniel <15kalin.d@gmail.com>
2014-11-02 19:12:36 -0800

    OIA-and-FizzBuzz

--- OnlyInAmerica_And_FizzBuzz.rb
+++ OnlyInAmerica_And_FizzBuzz.rb
@@ -24,11 +24,11 @@ comparator << comparator[2]
 
 sleep(1)
 if comparator[0] > comparator[1] && comparator[0] > comparator[2]
-	puts "21 is the greatest!"
+	puts comparator[0] + " is the greatest!"
 elsif comparator[1] > comparator[2] && comparator[1] > comparator[0]
-	puts "23 is the greatest!"
+	puts comparator[1] + " is the greatest!"
 elsif comparator[2] > comparator[0] && comparator[2] > comparator[1]
-	puts "25 is the greatest!"
+	puts comparator[2] + " is the greatest!"
 else 
 	puts "They're all equal!"
 end
@@ -39,19 +39,20 @@ values = ["Sphinx", "Rottweiler"]
 
 puts Hash[keys.zip(values)] # Kyle, teach us this please!
 
-=begin for i in 1..100
+for i in 0..99
 		i = i + 1
 	
-		if i/3.0.is_a? Integer
-			puts "Fizz!"
-		elsif i/5.0.is_a? Integer
+		if i%15.0 == 0
+			puts "Fizzbuzz!"
+			sleep(0.2)
+		elsif i%5.0 == 0
 			puts "Buzz!"
-		elsif i/15.0.is_a? Integer
-			puts "FizzBuzz!"
+			sleep(0.2)
+		elsif i%3.0 == 0
+			puts "Fizz!"
+			sleep(0.2)
+		else 
+			puts i
+			sleep(0.2)
 		end
-		
-		puts i
-	
-	=end end
-
-# Kyle, I really had NO IDEA as to how to do FizzBuzz. This is my best attempt. I ask you to please demonstrate the correct method of doing this.
+	end
